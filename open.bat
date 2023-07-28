477618c8aa6f72373fbb5ee4fd387f3dc2f16a4c
TechOGR <onelguilarte858@gmail.com>
2023-07-28 12:22:38 -0400

    Updating .bat

--- open.bat
+++ open.bat
@@ -1,4 +1,5 @@
 @echo off
 :: Remplacen [RutaMain] con la ruta al archivo main.py del repositorio
 :: Remplacen [RutaLog] con la ruta del archivo output
+:: Just do it
 start /B pythonw [RutaMain] > [RutaLog]
