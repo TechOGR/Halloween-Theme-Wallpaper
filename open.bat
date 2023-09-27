aa603c040038a7ecab34ccffe30b31fa9e9f47c3
TechOGR <onelguilarte858@gmail.com>
2023-09-27 02:49:07 -0400

    Deleting

--- open.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-:: Remplacen [RutaMain] con la ruta al archivo main.py del repositorio
-:: Remplacen [RutaLog] con la ruta del archivo output
-:: Just do it
-start /B pythonw [RutaMain] > [RutaLog]
